80dc149906578707ddda10e90c46f2c71d4ed0c9
Vaibhav <vaibhavraghuvanshi58@gmail.com>
2026-06-20 09:42:53 +0530

    updates

--- tests/amazon.spec.js
+++ tests/amazon.spec.js
@@ -1,15 +1,13 @@
-
-
 import { test, expect } from '@playwright/test';
 import items from '../data/amazon.json' with {type: "json"}
 
 
 for(const item of items){
-test(`amazon search for ${item}`, async ({ page }) => {
-  await page.goto('https://www.amazon.com/');
-  await page.getByRole('searchbox', { name: 'Search Amazon' }).click();
-  await page.getByRole('searchbox', { name: 'Search Amazon' }).fill(item);
-  await page.getByRole('searchbox', { name: 'Search Amazon' }).press('Enter');
-  await page.locator('.a-link-normal').first().click();
-});
+  test(`amazon search for ${item}`, async ({ page }) => {
+    await page.goto('https://www.amazon.com/');
+    await page.getByRole('searchbox', { name: 'Search Amazon' }).click();
+    await page.getByRole('searchbox', { name: 'Search Amazon' }).fill(item);
+    await page.getByRole('searchbox', { name: 'Search Amazon' }).press('Enter');
+    await page.locator('.a-link-normal').first().click();
+  });
 }
